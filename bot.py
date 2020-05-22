05dc5220072fbdf3d75eb3eac568ef91b355ce8f
alix <alice.bystrih@yandex.ru>
2020-05-23 01:14:20 +0300

    New skill: find out constellation of the object

--- bot.py
+++ bot.py
@@ -1,12 +1,28 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+import ephem
+from datetime import date
+
 import settings
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 PROXY = {'proxy_url': settings.PROXY_URL,
          'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
+
+
+def constellation(update, context):
+    name = update.message.text.split()[1].capitalize()
+
+    try:
+        space_object = getattr(ephem, name)(date.today().strftime('%Y/%m/%d'))
+        location = ephem.constellation(space_object)
+
+        update.message.reply_text(f'{name} is currently in the constellation of {location[1]}')
+
+    except AttributeError:
+        update.message.reply_text("I don't know, sorry")
 
 
 def greet_user(update, context):
@@ -25,6 +41,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('locate', constellation))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info('Bot has started')
